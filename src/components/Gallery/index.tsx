75247204d08ff34eef3b431945bed13127bc356a
TamaradeMoura <tamara_moura_13@hotmail.com>
2024-04-24 15:46:45 -0300

    novo commit

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,122 @@
+import { useState } from 'react'
+
+import Section from '../Section'
+
+import { Item, Action, Modal, ModalContent, Items } from './styles'
+
+import zelda from '../../assets/images/zelda.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import fechar from '../../assets/images/fechar.png'
+import { GalleryItem } from '../../pages/Home'
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=T-sXjIYOLKt4L1XS'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+  items: GalleryItem[]
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name, items }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {items.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Imagem ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a media"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={fechar}
+              alt="ícone de fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </ModalContent>
+        <div
+          onClick={() => {
+            closeModal()
+          }}
+          className="overlay"
+        ></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Game } from '../pages/Home'
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://fake-api-tau.vercel.app/api/eplay'
+  }),
+  endpoints: (builder) => ({
+    getFeaturedGame: builder.query<Game, void>({
+      query: () => 'destaque'
+    }),
+    getOnsale: builder.query<Game[], void>({
+      query: () => 'promocoes'
+    }),
+    getSoon: builder.query<Game[], void>({
+      query: () => 'em-breve'
+    })
+  })
+})
+
+export const { useGetFeaturedGameQuery, useGetOnsaleQuery, useGetSoonQuery } =
+  api
+
+export default api
